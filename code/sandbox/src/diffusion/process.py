fb58695edd500a40e1cf75a36dac7a481e645e54
Sausi9 <audunbergsson@gmail.com>
2026-03-04 19:09:18 +0100

    fixing gitignore and removing MNIST and python artficats from git index

--- code/sandbox/src/diffusion/process.py
+++ code/sandbox/src/diffusion/process.py
@@ -13,17 +13,18 @@ class Diffusion:
         alpha_bar_t = self.alpha_bar[t]
         #reparametrization formulation
         epsilon = torch.randn_like(x0)
-        q_sample = torch.sqrt(alpha_bar_t) + (1 - alpha_bar_t) * epsilon
+        q_sample = torch.sqrt(alpha_bar_t) * x0 + (1 - alpha_bar_t) * epsilon
         return q_sample
 
     def model_pred(self, model, x_t, t, alpha_bar_t):
         eps_pred = model(x_t, t)
         x0_pred = (x_t - torch.sqrt(1 - alpha_bar_t) * eps_pred) / torch.sqrt(alpha_bar_t)
-        return x0_pred
+        return x0_pred, eps_pred
 
     def p_mean_variance(self, model, x_t, t):
         eps_pred = model(x_t, t)
         mu_theta = (1/torch.sqrt(self.alphas[t])) * (x_t - (self.betas[t] / torch.sqrt(1 - self.alpha_bar[t])) * eps_pred)
+        var_theat = torch
         return mu_theta, 
     def reverse(self, model, x_t, x):
 
