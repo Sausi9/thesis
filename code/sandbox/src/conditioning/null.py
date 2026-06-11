70b6737c697e635ee33ab2dc98dbc1ebe7044f37
Sausi9 <audunbergsson@gmail.com>
2026-06-11 14:09:02 +0000

    bivariate basically done, also have swapped to overleaf for writing, more clean to see, pdf displays better

--- code/sandbox/src/conditioning/null.py
+++
@@ -1,7 +0,0 @@
-from src.conditioning.base import ConditioningPotential
-import torch
-
-
-class NullConditioner(ConditioningPotential):
-    def log_potential_x0(self, x0: torch.Tensor) -> torch.Tensor:
-        return torch.zeros(x0.shape[0], device=x0.device, dtype=x0.dtype)
